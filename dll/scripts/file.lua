2fbbb89d4227de9994c53e31075a0da24f543f97
Luaengine <94763918+Luaengine@users.noreply.github.com>
2021-11-25 13:59:36 +0000

    Fixed searchpath

--- dll/scripts/file.lua
+++ dll/scripts/file.lua
@@ -1,7 +1,7 @@
 file={}
 if scrupp.PLATFORM=="Windows" then
 	file.dirs=function(a)
-		local b=io.popen([[dir "]]..core.dir()..a..[[" /b /ad]])
+		local b=io.popen([[dir "]]..core.dir..a..[[" /b /ad]])
 		local c=b:read("*a")b:close()
 		return string.explode(c,"\n")
 	end
@@ -10,34 +10,34 @@ elseif scrupp.PLATFORM=="Linux" then
 elseif scrupp.PLATFORM=="Mac OS X" then
 else exit()end
 file.exists=function(a)
-	local b=io.open(core.dir()..a,"r")
+	local b=io.open(core.dir..a,"r")
 	if b~=nil then io.close(b)
 	return true else
 	return false end
 end
 file.read=function(a)
-	local b=io.open(core.dir()..a,"r")
+	local b=io.open(core.dir..a,"r")
 	if b~=nil then
 		local c=b:read("*a")
 		io.close(b)return c
 	else return""end
 end
 file.write=function(a,b)
-	local c=io.open(core.dir()..a,"w")
+	local c=io.open(core.dir..a,"w")
 	if c~=nil then
 		c:write(tostring(b))
 		io.close(c)
 	end
 end
 file.append=function(a,b)
-	local c=io.open(core.dir()..a,"a")
+	local c=io.open(core.dir..a,"a")
 	if c~=nil then
 		c:write(tostring(b))
 		io.close(c)
 	end
 end
 file.size=function(a)
-	local b=io.open(core.dir()..a,"r")
+	local b=io.open(core.dir..a,"r")
 	if b~=nil then
 		local c=b:seek("end")
 		io.close(b)return c
